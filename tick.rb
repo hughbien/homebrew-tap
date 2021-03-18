60e453a9e95c1b2226bbca2fb88724bfb199d3f0
Hugh Bien <hugh@hughbien.com>
2021-03-18 18:52:56 -0500

    Update tick to v0.1.1, fixes homebrew dwarf compile issue

--- tick.rb
+++ tick.rb
@@ -1,9 +1,9 @@
 class Tick < Formula
   desc "Command line stocks"
   homepage "https://hughbien.com/tick"
-  url "https://github.com/hughbien/tick/archive/v0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "5444ac6cbfcd0a2816aed2261ee2b714c44ea5ba6519e7d8cd1ca6078c2dc852"
+  url "https://github.com/hughbien/tick/archive/v0.1.1.tar.gz"
+  version "0.1.1"
+  sha256 "8f5c006de0bce96861ceeda4e32872beaed0293d4cd08c63f746eafe0db5e13e"
   head "https://github.com/hughbien/tick.git"
 
   depends_on "crystal" => [:build, :recommended, "0.36.1"]
