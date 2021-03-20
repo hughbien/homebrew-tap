9b7f6450e155178b4161a63a6ab73f885a49a727
Hugh Bien <hugh@hughbien.com>
2021-03-19 20:54:20 -0500

    mark v0.1.3

--- mark.rb
+++ mark.rb
@@ -1,9 +1,9 @@
 class Mark < Formula
   desc "Preview markdown in browser"
   homepage "https://hughbien.com/mark"
-  url "https://github.com/hughbien/mark/archive/v0.1.2.tar.gz"
-  version "0.1.2"
-  sha256 "0d9aabf1b8df6a69cab11419876f51ba50052653a72fd3ea838b2bf677a1d02a"
+  url "https://github.com/hughbien/mark/archive/v0.1.3.tar.gz"
+  version "0.1.3"
+  sha256 "2d215de7893460be46223f2fbee8e8eeccb9134f49204bb85e6d8ec839a4dd72"
   head "https://github.com/hughbien/mark.git"
 
   depends_on "crystal" => [:build, :recommended, "0.36.1"]
