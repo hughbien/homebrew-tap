4e41d5ee0cf35b9c5a681f960111946a24b71b42
Hugh Bien <hugh@hughbien.com>
2021-03-19 21:22:07 -0500

    vox v0.1.2

--- vox.rb
+++ vox.rb
@@ -1,9 +1,9 @@
 class Vox < Formula
   desc "Static site generator"
   homepage "https://hughbien.com/vox"
-  url "https://github.com/hughbien/vox/archive/v0.1.1.tar.gz"
-  version "0.1.1"
-  sha256 "121c2a829e7cd71acd66c429a6da1ef0c0b4bf2f3d001386f579bb1577c08e91"
+  url "https://github.com/hughbien/vox/archive/v0.1.2.tar.gz"
+  version "0.1.2"
+  sha256 "ce7ed420779e418a3565d6c285ce3cc249a9e13086bd0d1762f780db857fcc11"
   head "https://github.com/hughbien/vox.git"
 
   depends_on "crystal" => [:build, :recommended, "0.36.1"]
