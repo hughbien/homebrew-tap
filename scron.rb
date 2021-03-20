489fbf0ada4f5edb6151c9e71cbbe5abb1280b00
Hugh Bien <hugh@hughbien.com>
2021-03-19 21:05:33 -0500

    scron v0.1.2

--- scron.rb
+++ scron.rb
@@ -1,9 +1,9 @@
 class Scron < Formula
   desc "Scheduler for laptops"
   homepage "https://hughbien.com/scron"
-  url "https://github.com/hughbien/scron/archive/v0.1.1.tar.gz"
-  version "0.1.1"
-  sha256 "34568403574994e19753c16d978c1a30a0f74236b60d7fd4791afe504449e161"
+  url "https://github.com/hughbien/scron/archive/v0.1.2.tar.gz"
+  version "0.1.2"
+  sha256 "99602343b432488b6e382192054f3445694de1bcb4b317ebfcb177d24435c611"
   head "https://github.com/hughbien/scron.git"
 
   depends_on "crystal" => [:build, :recommended, "0.36.1"]
