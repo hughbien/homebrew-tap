ca1d8e122b7658afc73ddd6209d5bcd2a21fd2d6
Hugh Bien <hugh@hughbien.com>
2021-03-19 20:36:26 -0500

    tick v0.1.2

--- tick.rb
+++ tick.rb
@@ -1,9 +1,9 @@
 class Tick < Formula
   desc "Command line stocks"
   homepage "https://hughbien.com/tick"
-  url "https://github.com/hughbien/tick/archive/v0.1.1.tar.gz"
-  version "0.1.1"
-  sha256 "8f5c006de0bce96861ceeda4e32872beaed0293d4cd08c63f746eafe0db5e13e"
+  url "https://github.com/hughbien/tick/archive/v0.1.2.tar.gz"
+  version "0.1.2"
+  sha256 "30f2468e5ff0d5db7b70327db2982383085818b03aec343db832b45cd2232db8"
   head "https://github.com/hughbien/tick.git"
 
   depends_on "crystal" => [:build, :recommended, "0.36.1"]
