b54f60c044f1b779f1623a6f02ab4018fa65b548
Hugh Bien <hugh@hughbien.com>
2021-06-03 11:41:12 -0500

    Actual thyme v0.1.6

--- thyme.rb
+++ thyme.rb
@@ -1,9 +1,9 @@
 class Thyme < Formula
   desc "Pomodoro timer for tmux"
   homepage "https://hughbien.com/thyme"
-  url "https://github.com/hughbien/thyme/archive/v0.1.5.tar.gz"
-  version "0.1.5"
-  sha256 "124b5cbb0ced9dda4f96bfa312b1d6945933310ebf81d36afbaa2727f5d0ee02"
+  url "https://github.com/hughbien/thyme/archive/v0.1.6.tar.gz"
+  version "0.1.6"
+  sha256 "684e679ba84177337fde2ae5c0db6d4555fb1d55dfde52b1b8343efcba75a261"
   head "https://github.com/hughbien/thyme.git"
 
   depends_on "crystal" => [:build, :recommended, "1.0.0"]
